2cc509a439e5bfbb3d0f4ca8a4b323a78f43ddda
so22975 <lumpen0130@hotmail.co.jp>
2026-05-07 15:07:16 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // このファイルを config.js にリネームして、Supabaseを使う場合だけ値を入れてください。
 // 空欄のままならブラウザ内保存モードで動きます。
 window.APP_CONFIG = {
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
-  APP_URL: ''
+  SUPABASE_URL: "https://wkmsjzozfdsqvzxnpazu.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrbXNqem96ZmRzcXZ6eG5wYXp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2MDg5OTQsImV4cCI6MjA5MzE4NDk5NH0.-KtK6RRTI12qoxAINxtmeB8hvNCNoldrP0NQG7PYDaE",
+  APP_URL: 'https://so22975.github.io/QRChecker/'
 };
